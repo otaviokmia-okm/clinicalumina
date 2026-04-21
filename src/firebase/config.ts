1e656ab370f06b7888ac4ad65843321d7aadb187
OTAVIO KMIA <otaviokmia@gmail.com>
2026-04-21 04:37:22 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8726152994-dcf80",
+  "appId": "1:828011797690:web:a2429f9131e60a71657683",
+  "apiKey": "AIzaSyAlqsiSHyCc20Fwf4eoV6sZPA4EFbpQ03k",
+  "authDomain": "studio-8726152994-dcf80.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "828011797690"
+};
